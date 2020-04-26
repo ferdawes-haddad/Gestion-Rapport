8f8ccc5692c7c42acefb4f558b1251d7eebc4d7f
Ferdawes Haddad <ferdawes.haddad@sesame.com.tn>
2020-04-26 04:01:49 +0200

    crud etudiant et enseignant

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -12,8 +12,14 @@ class AdminController extends AbstractController
      */
     public function index()
     {
-        return $this->render('admin/index.html.twig', [
-            'controller_name' => 'AdminController',
-        ]);
+        return $this->render('admin/index.html.twig', ['controller_name' => 'AdminController',]);
+    }
+
+    /**
+     * @Route("/ajoutEtudiant", name="admin")
+     */
+    public function AjoutEtudiant()
+    {
+        return $this->render('admin/AjoutEtudiant.html.twig', ['controller_name' => 'AdminController',]);
     }
 }
